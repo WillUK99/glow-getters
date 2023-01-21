8a4a99fadee6222d4063303196bc29e537273c68
WillUK99 <76099444+WillUK99@users.noreply.github.com>
2023-01-21 12:16:14 +0000

    chore: add fonts back, remove footer

--- src/lib/components/layout/Footer.svelte
+++ src/lib/components/layout/Footer.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 </script>
 
-<footer class="container flex flex-col px-2 mx-auto mb-5 gap-7 font-rowdies">
+<!-- <footer class="container flex flex-col px-2 mx-auto mb-5 gap-7 font-rowdies">
 	<a class="text-4xl font-bold hover:text-tertiary" href="/articles">Articles</a>
-</footer>
+</footer> -->
